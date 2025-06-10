4da10d8b981517aed959cfa75749aa3dbd7d9246
Ahmad <ahmadfaidh2003@gmail.com>
2025-06-10 19:15:21 +0700

    balik lagi

--- api/index.js
+++ api/index.js
@@ -10,7 +10,6 @@ import reportRoute from './routes/report.route.js';
 import paymentRoutes from './routes/payment.route.js';
 import dayReportRoute from './routes/dayReport.route.js';
 import storeStatusRoute from './routes/storeStatus.route.js';
-import path from 'path';
 
 
 dotenv.config();
@@ -22,8 +21,6 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => {
     console.log(err);
 });
-
-const __dirname = path.resolve();
 
 const app = express();
 
@@ -42,12 +39,6 @@ app.use('/api/report', reportRoute);
 app.use('/api/payment', paymentRoutes);
 app.use('/api/dayReport', dayReportRoute);
 app.use('/api/store', storeStatusRoute);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
